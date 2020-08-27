d460a040419fbbad0ab4cb9217afb38dcd931ce1
Victor Chan <victorcnk@gmail.com>
2020-08-27 22:24:02 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,30 @@
 'use strict';
 
-/*const placeholders = ['Chinese..', 'Mexican..', 'American..', 'Mediterranean..', 'Southern..',
-    'French..', 'Greek..'];
-let counter = 0;
-let inquire = document.getElementById('cuisine-input');
-let length = placeholders.length;
-
-function changePlaceholder() {
-    if (counter >= length) {
-        counter = 0;
-    }
-    inquire.setAttribute('placeholder', placeholders[counter]);
-    counter++;
-}
-
-setInterval(changePlaceholder, 3000);
-
-}
-*/
 function displayWineResults(responseJson, searchTerm) {
-    $('#results-list').empty();
     const paired = [];
     for (let i = 0; i < responseJson.pairedWines.length; i++) {
         paired.push(`<li>${responseJson.pairedWines[i]}</li>`);
     }
-    $('#results-list').append(
-        `
-        <li><h2>${responseJson.productMatches[0].title}</h2>
+    console.log("Value of paired is " + paired);
+    $('#itemName').html(`
+        <h2>${responseJson.productMatches[0].title}</h2>
         <a href="${responseJson.productMatches[0].link}" target="_blank"><img src="${responseJson.productMatches[0].imageUrl}" 
         alt="Photo of ${responseJson.productMatches.title}"/>
-        </a></li>
+        </a>`)
+
+    $('#pairedWine').html(`
         <ul class="paired">
         <h3>Paired Wines:</h3>
         ${paired.join('')}
-        </ul>
-        <li>
+        </ul>`)
+
+    $('#descriptionText').html(`
+        
         <pre><h3>Wine:</h3>${responseJson.productMatches[0].title}</pre>
         <pre><h3>Description:</h3>${responseJson.productMatches[0].description}</pre>
         <pre><h3>Details:</h3>${responseJson.pairingText}</pre>
-        </li>`
-    )
+        `)
+
     $('#results').removeClass('hidden');
 }
 
@@ -47,22 +32,13 @@ function watchFoodButton() {
     $('section').on('click', '#food-submit', event => {
         event.preventDefault();
         const searchTerm = $('#food-input').val();
+        console.log('searchTerm is ' + searchTerm);
         fetch(`https://api.spoonacular.com/food/wine/pairing?food=${searchTerm}&apiKey=27b52f3cd6fc4b49a9d3460e3f593f0b`)
-            .then(response => response.json())
+            .then(response => {let answer = response.json(); console.log(answer); return answer;})
             .then(responseJson => displayWineResults(responseJson,searchTerm))
-            .catch(error => console.log('Food went wrong')
+            .catch(error => {console.log('Food went wrong. Error was ' + error); alert(error + " - Please try another food");}
             );
     });
 }
-
-// function watchRandomButton() {
-//     $('section').on('click', '#random-meal', event => {
-//         event.preventDefault();
-//         fetch('https://api.spoonacular.com/recipes/random?apiKey=27b52f3cd6fc4b49a9d3460e3f593f0b')
-//             .then(response => response.json())
-//             .then(responseJson => displayRandomResults(responseJson))
-//             .catch(error => console.log('Something went wrong'));
-//     });
-// }
 
 $(watchFoodButton);
