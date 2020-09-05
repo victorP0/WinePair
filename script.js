46cfda44ba2128756d32517fb967dee6473f00fb
Victor Chan <victorcnk@gmail.com>
2020-09-05 19:19:51 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const key = "27b52f3cd6fc4b49a9d3460e3f593f0b"
+
 function displayDishResults(paired) {
-fetch(`https://api.spoonacular.com/food/wine/dishes?wine=${paired}&apiKey=27b52f3cd6fc4b49a9d3460e3f593f0b`)
+fetch(`https://api.spoonacular.com/food/wine/dishes?wine=${paired}&apiKey=${key}`)
             .then(response => response.json())
             .then(responseJson => getDish(responseJson))
             .catch(error => console.log('Cuisine went wrong'));
@@ -21,7 +23,6 @@ function displayWineResults(responseJson, searchTerm) {
                       <span class="wineText"></span>
                       </li>`);
     }
-    console.log("Value of paired is " + paired);
 
     $('#itemName').html(`
         <h2>${responseJson.productMatches[0].title}</h2>
@@ -38,7 +39,7 @@ function displayWineResults(responseJson, searchTerm) {
     $('.wine-description').hover( function() {
 
       if ($(this).children('.wineText').html() === '' ){
-        fetch(`https://api.spoonacular.com/food/wine/dishes?wine=${$(this).children('.wine-name').text()}&apiKey=27b52f3cd6fc4b49a9d3460e3f593f0b`)
+        fetch(`https://api.spoonacular.com/food/wine/dishes?wine=${$(this).children('.wine-name').text()}&apiKey=${key}`)
         .then(response => response.json())
         .then(responseJson => {
                   $(this).children('.wineText').html(("<toolTip>" + "<li>" + responseJson['text'] + "</li>" + "</toolTip>" )); 
@@ -78,19 +79,22 @@ function errorMessage (){
     $('#results').removeClass('hidden');
 }
 
-
-
 function watchFoodButton() {
     $('section').on('click', '#food-submit', event => {
         event.preventDefault();
         const searchTerm = $('#food-input').val();
-        console.log('searchTerm is ' + searchTerm);
         fetch(`https://api.spoonacular.com/food/wine/pairing?food=${searchTerm}&apiKey=27b52f3cd6fc4b49a9d3460e3f593f0b`)
             .then(response => response.json())
             .then(responseJson => displayWineResults(responseJson,searchTerm))
-            .catch(error => {console.log('Food went wrong. Error was ' + error); errorMessage();}
+            .catch(error => {errorMessage();}
             );
     });
 }
 
+  $('section').keyup(function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   $('#food-submit').click();  
+   }
+});  
 $(watchFoodButton);
